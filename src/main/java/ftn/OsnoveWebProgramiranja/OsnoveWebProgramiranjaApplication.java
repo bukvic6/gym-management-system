186f13a59e1bd6105f494f10c35a224289cc09ce
bukvic6 <bukvic@gmail.com>
2022-01-18 16:07:37 +0100

    Initial commit

---
+++ src/main/java/ftn/OsnoveWebProgramiranja/OsnoveWebProgramiranjaApplication.java
@@ -0,0 +1,13 @@
+package ftn.OsnoveWebProgramiranja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsnoveWebProgramiranjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsnoveWebProgramiranjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ftn/OsnoveWebProgramiranja/OsnoveWebProgramiranjaApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.OsnoveWebProgramiranja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsnoveWebProgramiranjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
